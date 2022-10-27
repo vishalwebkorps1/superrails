16f1e935bce12c8610ec56fd74b438be5157bca7
vishal Thakur <vish75810@gmail.com>
2022-10-27 23:50:48 +0530

    static public controller with pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_public#landing_page'
+  get 'privacy', to: 'static_public#privacy'
+  get  'terms', to: 'static_public#terms'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
